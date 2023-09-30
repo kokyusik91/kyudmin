fc078508bea21ad17608b09d3729ca01a847bea1
kyusikko <kokyusik91@naver.com>
2023-09-30 18:05:18 +0900

    ✨ 유저에게 상점 이름을 입력받아, db에 넣는 기능 개발

--- components/modals/store-modal.tsx
+++ components/modals/store-modal.tsx
@@ -1,10 +1,56 @@
 'use client';
 
+import { useState } from 'react';
+import * as z from 'zod';
+import { useForm } from 'react-hook-form';
+
 import { useStoreModal } from '@/hooks/use-store-modal';
 import Modal from '@/components/ui/modal';
+import { zodResolver } from '@hookform/resolvers/zod';
+
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from '@/components/ui/form';
+import { Input } from '@/components/ui/input';
+import { Button } from '@/components/ui/button';
+import axios from 'axios';
+import { toast } from 'react-hot-toast';
+
+// 무언가 스키마를 만들어주고, react-hook-form에 연결해준다.
+const formSchema = z.object({
+  name: z.string().min(1),
+});
 
 export default function StoreModal() {
   const storeModal = useStoreModal();
+
+  const [loading, setLoading] = useState(false);
+
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      name: '',
+    },
+  });
+
+  const onSubmit = async (values: z.infer<typeof formSchema>) => {
+    try {
+      setLoading(true);
+      const response = await axios.post('/api/stores', values);
+      toast.success('Store created!');
+    } catch (error) {
+      toast.error('Something went wrong.');
+      console.log(error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
   return (
     <Modal
       title='Create Store'
@@ -12,7 +58,43 @@ export default function StoreModal() {
       isOpen={storeModal.isOpen}
       onClose={storeModal.onClose}
     >
-      Body!
+      <div>
+        <div className='space-y-4 py-2 pb-4'>
+          <Form {...form}>
+            <form onSubmit={form.handleSubmit(onSubmit)}>
+              <FormField
+                control={form.control}
+                name='name'
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Name</FormLabel>
+                    <FormControl>
+                      {/* ...field안에 OnChange, blur, value, name이 들어가있다. */}
+                      <Input
+                        disabled={loading}
+                        placeholder='E-Commerce'
+                        {...field}
+                      />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <div className='pt-6 space-x-2 flex items-center justify-end w-full'>
+                <Button
+                  disabled={loading}
+                  variant='outline'
+                  onClick={storeModal.onClose}
+                >
+                  Cancel
+                </Button>
+                {/* onClick이 아니고 form안에 있는 버튼이므로, onSubmit이 실행된다. */}
+                <Button type='submit'>Continue</Button>
+              </div>
+            </form>
+          </Form>
+        </div>
+      </div>
     </Modal>
   );
 }
