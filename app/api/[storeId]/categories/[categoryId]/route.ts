460410b6f4e38a15761e43fcd1ee91c3b41f6770
kyusikko <kokyusik91@naver.com>
2023-10-01 15:52:42 +0900

    ✨ 카테고리 기능 개발 (billboard 폴더 재사용후 고침)

---
+++ app/api/[storeId]/categories/[categoryId]/route.ts
@@ -0,0 +1,121 @@
+import prismadb from '@/lib/prismadb';
+import { auth } from '@clerk/nextjs';
+import { NextResponse } from 'next/server';
+
+// 특정 빌보드 가져오기
+export async function GET(
+  req: Request,
+  { params }: { params: { categoryId: string } }
+) {
+  try {
+    if (!params.categoryId) {
+      return new NextResponse('Category ID is required', { status: 400 });
+    }
+
+    const category = await prismadb.category.findUnique({
+      where: {
+        id: params.categoryId,
+      },
+    });
+
+    return NextResponse.json(category);
+  } catch (error) {
+    console.log('[CATEGORY_GET]', error);
+    return new NextResponse('Internal Error', { status: 500 });
+  }
+}
+
+// 특정 빌보드 수정
+export async function PATCH(
+  req: Request,
+  { params }: { params: { storeId: string; categoryId: string } }
+) {
+  try {
+    const { userId } = auth();
+    const body = await req.json();
+
+    const { name, billboardId } = body;
+
+    if (!userId) {
+      return new NextResponse('Unauthenticated', { status: 401 });
+    }
+
+    if (!name) {
+      return new NextResponse('Name is required', { status: 400 });
+    }
+
+    if (!billboardId) {
+      return new NextResponse('BillboardId is required', { status: 400 });
+    }
+
+    if (!params.categoryId) {
+      return new NextResponse('CategoryId Id is required', { status: 400 });
+    }
+
+    const storeByUserId = await prismadb.store.findFirst({
+      where: {
+        id: params.storeId,
+        userId,
+      },
+    });
+    // 수정할 권한이 없음을 던져 준다.
+    if (!storeByUserId) {
+      return new NextResponse('Unauthorized', { status: 403 });
+    }
+
+    const category = await prismadb.category.updateMany({
+      where: {
+        id: params.categoryId,
+      },
+      data: {
+        name,
+        billboardId,
+      },
+    });
+
+    return NextResponse.json(category);
+  } catch (error) {
+    console.log('[CATEGORY_PATCH]', error);
+    return new NextResponse('Internal Error', { status: 500 });
+  }
+}
+
+// 특정 빌보드 삭제
+export async function DELETE(
+  req: Request,
+  { params }: { params: { storeId: string; categoryId: string } }
+) {
+  try {
+    const { userId } = auth();
+
+    if (!userId) {
+      return new NextResponse('Unauthenticated', { status: 401 });
+    }
+
+    if (!params.categoryId) {
+      return new NextResponse('category ID is required', { status: 400 });
+    }
+
+    const storeByUserId = await prismadb.store.findFirst({
+      where: {
+        id: params.storeId,
+        userId,
+      },
+    });
+    // 수정할 권한이 없음을 던져 준다.
+    if (!storeByUserId) {
+      return new NextResponse('Unauthorized', { status: 403 });
+    }
+
+    const category = await prismadb.category.deleteMany({
+      where: {
+        id: params.categoryId,
+      },
+    });
+
+    return NextResponse.json(category);
+  } catch (error) {
+    console.log('[CATEGORY_DELETE]', error);
+    return new NextResponse('Internal Error', { status: 500 });
+  }
+}
