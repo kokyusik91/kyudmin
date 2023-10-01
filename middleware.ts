1e4b2dfb9727620f241d66707a0c735ffa3af46f
kyusikko <kokyusik91@naver.com>
2023-10-01 14:47:35 +0900

    ✨ 로컬환경에서의 auth가 필요없는 api들은 해제 시켜줄 수 있음.

--- middleware.ts
+++ middleware.ts
@@ -3,7 +3,10 @@ import { authMiddleware } from '@clerk/nextjs';
 // This example protects all routes including api/trpc routes
 // Please edit this to allow other routes to be public as needed.
 // See https://clerk.com/docs/references/nextjs/auth-middleware for more information about configuring your middleware
-export default authMiddleware({});
+export default authMiddleware({
+  // api-routes의 auth를 해제 해주는 듯... 로컬환경에서만
+  publicRoutes: ['/api/:path*'],
+});
 
 export const config = {
   matcher: ['/((?!.+\\.[\\w]+$|_next).*)', '/', '/(api|trpc)(.*)'],
