c0fc4fadf437e501931941ab06ebcf0caed8943e
kyusikko <kokyusik91@naver.com>
2023-10-01 17:55:17 +0900

    ✨ products api routes (개복잡함...)

---
+++ app/api/[storeId]/products/route.ts
@@ -0,0 +1,136 @@
+import { auth } from '@clerk/nextjs';
+import { NextResponse } from 'next/server';
+
+import prismadb from '@/lib/prismadb';
+
+export async function POST(
+  req: Request,
+  { params }: { params: { storeId: string } }
+) {
+  try {
+    const { userId } = auth();
+    const body = await req.json();
+
+    const {
+      name,
+      price,
+      categoryId,
+      sizeId,
+      colorId,
+      images,
+      isFeatured,
+      isArchived,
+    } = body;
+
+    if (!userId) {
+      return new NextResponse('Unauthenticated', { status: 401 });
+    }
+
+    if (!name) {
+      return new NextResponse('Name is required', { status: 400 });
+    }
+
+    if (!images || !images.length) {
+      return new NextResponse('Images are required', { status: 400 });
+    }
+
+    if (!price) {
+      return new NextResponse('Price is required', { status: 400 });
+    }
+
+    if (!categoryId) {
+      return new NextResponse('CategoryId is required', { status: 400 });
+    }
+
+    if (!sizeId) {
+      return new NextResponse('sizeId is required', { status: 400 });
+    }
+
+    if (!colorId) {
+      return new NextResponse('colorId is required', { status: 400 });
+    }
+
+    if (!params.storeId) {
+      return new NextResponse('StoreId  is required', { status: 400 });
+    }
+
+    const storeByUserId = await prismadb.store.findFirst({
+      where: {
+        id: params.storeId,
+        userId,
+      },
+    });
+    // 수정할 권한이 없음을 던져 준다.
+    if (!storeByUserId) {
+      return new NextResponse('Unauthorized', { status: 403 });
+    }
+
+    const product = await prismadb.product.create({
+      data: {
+        name,
+        price,
+        isArchived,
+        isFeatured,
+        categoryId,
+        colorId,
+        sizeId,
+        storeId: params.storeId,
+        images: {
+          createMany: {
+            data: [...images.map((image: { url: string }) => image)],
+          },
+        },
+      },
+    });
+    // 성공시!
+    return NextResponse.json(product);
+  } catch (error) {
+    // development에서는 console만 찍자. production에서는 sentry
+    console.log('[PRODUCTS_POST]', error);
+    return new NextResponse('Internal Error', { status: 500 });
+  }
+}
+
+// 모든 빌보드 GET 하기
+export async function GET(
+  req: Request,
+  { params }: { params: { storeId: string } }
+) {
+  try {
+    const { searchParams } = new URL(req.url);
+    const categoryId = searchParams.get('categoryId') || undefined;
+    const colorId = searchParams.get('colorId') || undefined;
+    const sizeId = searchParams.get('sizeId') || undefined;
+    const isFeatured = searchParams.get('isFeatured');
+
+    if (!params.storeId) {
+      return new NextResponse('StoreId  is required', { status: 400 });
+    }
+
+    const products = await prismadb.product.findMany({
+      where: {
+        storeId: params.storeId,
+        categoryId,
+        colorId,
+        sizeId,
+        isFeatured: isFeatured ? true : undefined,
+        isArchived: false,
+      },
+      include: {
+        images: true,
+        category: true,
+        color: true,
+        size: true,
+      },
+      orderBy: {
+        createdAt: 'desc',
+      },
+    });
+    // 성공시!
+    return NextResponse.json(products);
+  } catch (error) {
+    // development에서는 console만 찍자. production에서는 sentry
+    console.log('[PRODUCTS_POST]', error);
+    return new NextResponse('Internal Error', { status: 500 });
+  }
+}
